391856704ff7482f0e1eb17615d21c674f6b4d41
Vlad Agiyevich <sky-mart@hotmail.com>
2015-10-05 14:50:09 +0300

    Папка логов. Подобрал коэффициенты в объекте

--- Matlab/podbor.m
+++ Matlab/podbor.m
@@ -1,21 +1,31 @@
-dz = 0.5;
-w0 = 7.39;
-b = w0^2;
+logName = '../Logs/imp1.txt';
+
+logFile = fopen(logName, 'r');
+M = fscanf(logFile, '%f %f %f %d %d %d %d %d %d %d %f %f %f %d %f \n', [15 Inf]);
+angle = M(1,:);
+vel = M(2,:);
+time = 0:0.01:(length(angle)-1) * 0.01;
+
+angle = - angle;
+vel = - vel;
+
+dz = 0.2;
+w0 = 7.4;
+b = 0.01;
 
 A = [0 1;
     -w0^2 -2*dz*w0];
 
-sys = ss(A, [0; b], [1 0], 0);
-impulse(sys)
-% bode(sys);
-% grid
+sys = ss(A, [0; b * w0^2], [1 0], 0);
+bode(sys);
 
-t = 0:0.01:3;
-u = zeros(1, length(t));
+controller = pid(20, 0, 0.1, 0);
 
-x0 = [0.35, 0.35];
-
-x = lsim(sys, u, t, x0);
-
-
-%plot(t, x);
+bode(tf(controller) * tf(sys));
+% subplot(2, 1, 1); 
+% impulse(sys);
+% 
+% subplot(2, 1, 2); 
+% plot(time, angle);
+% ylabel('Angle, rads');
+% xlabel('Time, s');
